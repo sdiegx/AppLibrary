150ba96e7c327db2a3ae2b5bbe91ad6bc15dbf27
sdiegx <zebastian7616@gmail.com>
2024-02-19 02:16:48 -0500

    Antes de agregar el dto

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
+
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>
+  ) { }
+
+  async create(createBookDto: CreateBookDto) {
     return 'This action adds a new book';
   }
 
-  findAll() {
-    return `This action returns all books`;
+  async findAll() {
+    return await this.bookRepository.find();
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} book`;
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
+  async update(id: number, updateBookDto: UpdateBookDto) {
     return `This action updates a #${id} book`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} book`;
   }
 }

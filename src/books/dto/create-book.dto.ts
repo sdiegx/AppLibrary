5d274d9a2a11c520264ea1e49178e4e84f484dee
sdiegx <zebastian7616@gmail.com>
2024-02-19 17:25:48 -0500

    Agregando validaciones personalizadas en el dto (data transfer object)

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,47 @@
-export class CreateBookDto {}
+import { IsBoolean, IsDate, IsInt, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+import { IsArrayOfStrings } from "../validators/arrayOfStrings.validator";
+
+export class CreateBookDto {
+    @IsString()
+    @MinLength(25)
+    title: string;
+
+    @IsString()
+    @IsArrayOfStrings()
+    authors: string[];
+
+    @IsString()
+    @MinLength(25)
+    publisher: string;
+
+    @IsDate()
+    publicationDate: Date;
+
+    @IsString()
+    @MinLength(15)
+    genre: string;
+    
+    @IsString()
+    @MinLength(100)
+    @IsOptional()
+    summary?: string;
+
+    @IsString()
+    @MinLength(15)
+    language: string;
+
+    @IsInt()
+    @IsPositive()
+    pageCount: number;
+
+    @IsString()
+    @MinLength(50)
+    physicalLocation: string;
+
+    @IsBoolean()
+    availabe: boolean;
+
+    @IsInt()
+    @IsPositive()
+    numberOfCopies: number;
+}
---
+++ src/books/validators/arrayOfStrings.validator.ts
@@ -0,0 +1,21 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export const IsArrayOfStrings = (validationOptions?: ValidationOptions) => (object: Object, propertyName: string) => {
+    registerDecorator({
+        name: 'isArrayOfStrings',
+        target: object.constructor,
+        propertyName: propertyName,
+        options: validationOptions,
+        validator: {
+            validate(value: any, args: ValidationArguments) {
+                if (!Array.isArray(value)) {
+                    return false;
+                }
+                return value.every(item => typeof item === 'string');
+            },
+            defaultMessage(args: ValidationArguments) {
+                return `${args.property} must be an array of strings`;
+            }
+        }
+    });
+};

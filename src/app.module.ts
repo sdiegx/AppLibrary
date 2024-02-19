134193529b49fb3ed3582c58565a1e681e4fe45e
sdiegx <zebastian7616@gmail.com>
2024-02-19 02:00:17 -0500

    Agregando los campos y tipos de datos de books

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, BooksModule],
+  imports: [
+    UsersModule, 
+    BooksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'root',
+      database: 'library_db',
+      autoLoadEntities: true, // esta linea hace que se carguen todas las entidades automaticamente 
+      // entities: [], en produccion quiza sea mejor poner las entidades una por una en este array en lugar de autoLoadEntities
+      synchronize: true, // tampoco es bueno usar en produccion porque se pueden perder datos
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,59 @@
-export class Book {}
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({ type: 'text', array: true, nullable: true })
+    authors: string[]; // Columna de tipo texto que almacena un array de autores
+
+    @Column()
+    publisher: string;
+
+    @Column()
+    publicationDate: Date;
+
+    @Column()
+    genre: string;
+
+    @Column()
+    summary?: string; // El resumen es opcional
+
+    @Column()
+    language: string;
+
+    @Column()
+    pageCount: number;
+
+    @Column()
+    physicalLocation: string;
+
+    @Column()
+    available: boolean;
+
+    @Column()
+    numberOfCopies: number;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+
+    // Método para serializar los autores a una cadena antes de guardar en la base de datos
+    serializeAuthors() {
+        if (this.authors) {
+            return JSON.stringify(this.authors);
+        }
+        return null;
+    }
+
+    // Método para deserializar la cadena de autores a un array después de recuperar de la base de datos
+    deserializeAuthors(authorsString: string) {
+        if (authorsString) {
+            return JSON.parse(authorsString);
+        }
+        return null;
+    }
+}

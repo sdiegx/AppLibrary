0ed9bdad099d5efc39b89d0390e2914eb48e9cdc
sdiegx <zebastian7616@gmail.com>
2024-02-20 10:46:48 -0500

    Agregando credenciales nuevas para la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,31 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [
     UsersModule, 
     BooksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'root',
-      database: 'library_db',
-      autoLoadEntities: true, // esta linea hace que se carguen todas las entidades automaticamente 
-      // entities: [], en produccion quiza sea mejor poner las entidades una por una en este array en lugar de autoLoadEntities
-      synchronize: true, // tampoco es bueno usar en produccion porque se pueden perder datos
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        retryDelay: 3000,
+        autoLoadEntities: true, // esta linea hace que se carguen todas las entidades automaticamente 
+        // entities: [], en produccion quiza sea mejor poner las entidades una por una en este array en lugar de autoLoadEntities
+        synchronize: true, // tampoco es bueno usar en produccion porque se pueden perder datos
+      }),
     }),
   ],
   controllers: [],

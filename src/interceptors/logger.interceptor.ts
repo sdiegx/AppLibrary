c9897086b18fc73bd3173214b57c1d2eae09dc30
sdiegx <zebastian7616@gmail.com>
2024-03-08 16:49:08 -0500

    Agregando la clase LoggerInterceptors

---
+++ src/interceptors/logger.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { tap } from 'rxjs';
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const startTime = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const endTime = Date.now();
+        const resTime = endTime - startTime;
+
+        console.log(
+          `${request.method} ${request.path} ${response.statusCode} ${resTime}ms`,
+        );
+      }),
+    );
+  }
+}
